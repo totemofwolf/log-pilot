28bdf66e1c4b5110d6aa823e6411a75dfb844f79
totemofwolf <gk.wl@qq.com>
2020-06-23 11:46:20 +0800

    Changes: build-image 脚本支持 设置自定义 tag

--- build-image.sh
+++ build-image.sh
@@ -3,18 +3,23 @@
 # build docker image
 #
 
+name="log-pilot"
+version="0.9.7"
+
 build()
 {
-    echo -e "building image: log-pilot:latest\n"
+    echo -e "building image: $name:$tag\n"
 
-    docker build -t log-pilot:latest -f Dockerfile.$1 .
+    docker build -t $name:$tag -f Dockerfile.$1 .
 }
 
 case $1 in
 fluentd)
+    tag="${version}-$1"
     build fluentd
     ;;
 *)
+    tag="${version}-filebeat"
     build filebeat
     ;;
 esac
